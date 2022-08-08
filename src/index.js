9f20615ff86fa9bced4bca6ab90d8a0b23c9cdaf
kimnewbie <gracecancho@gmail.com>
2022-08-08 17:42:22 +0900

    test

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from './modules/counter';
 import todosReducer from './modules/todos';
+import { logger } from 'redux-logger';
 
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
     todos: todosReducer,
+    middleware: [logger],
   },
 });
 
